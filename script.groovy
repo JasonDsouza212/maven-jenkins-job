b57fdb6456b13fbe604a7054b74f4c7b79f6031e
Jason <jasondsouza212@gmail.com>
2022-11-24 19:16:24 +0530

    groovy script file added'

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+    echo 'building the app in groovy script'
+}
+def buildTest() {
+    echo 'Testing the app in groovy script'
+}
+
+def buildDeploy() {
+    echo 'Deploy the app in groovy script'
+}
+
+
+return this
