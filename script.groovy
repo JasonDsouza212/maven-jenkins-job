6a96e59b0f8a2d7a20fae3211b6225bb2231ad79
Jason <jasondsouza212@gmail.com>
2022-11-25 11:04:51 +0530

    nothing jst created a groovy script

--- script.groovy
+++ script.groovy
@@ -8,6 +8,16 @@ def buildTest() {
 def buildDeploy() {
     echo 'Deploy the app in groovy script'
 }
-
-
+def buildJar() {
+    echo "building the app"
+    sh "mvn package"
+}
+def buildImage() {
+     echo "building the docker image"
+     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable:'PASS', usernameVariable: 'USER' )]){
+     sh 'docker build -t jasonkd006/my-repo:jma-2.0 .'
+     sh "echo $PASS | docker login -u $USER --password-stdin"
+     sh 'docker push jasonkd006/my-repo:jma-2.0'
+     }
+}
 return this
