e4cc0268a33bdf93984c865d58c861c31bbf1dc4
Jason <jasondsouza212@gmail.com>
2022-11-26 18:33:51 +0530

    bulding version number automatically

--- script.groovy
+++ script.groovy
@@ -10,14 +10,14 @@ def buildDeploy() {
 }
 def buildJar() {
     echo "building the app"
-    sh "mvn package"
+    sh "mvn clean package"
 }
 def buildImage() {
      echo "building the docker image"
      withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable:'PASS', usernameVariable: 'USER' )]){
-     sh 'docker build -t jasonkd006/my-repo:jma-2.0 .'
+     sh "docker build -t jasonkd006/my-repo:${IMAGE_NAME} ."
      sh "echo $PASS | docker login -u $USER --password-stdin"
-     sh 'docker push jasonkd006/my-repo:jma-2.0'
+     sh "docker push jasonkd006/my-repo:${IMAGE_NAME}"
      }
 }
 return this
