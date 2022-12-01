ae8514eb654503fab343262f4fe9be60d78bb6b7
Jason Dsouza <74404894+JasonDsouza212@users.noreply.github.com>
2022-12-01 13:48:46 +0530

    Update script.groovy
    new jenkins id updated

--- script.groovy
+++ script.groovy
@@ -14,7 +14,7 @@ def buildJar() {
 }
 def buildImage() {
      echo "building the docker image"
-     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable:'PASS', usernameVariable: 'USER' )]){
+     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable:'PASS', usernameVariable: 'USER' )]){
      sh "docker build -t jasonkd006/my-repo:${IMAGE_NAME} ."
      sh "echo $PASS | docker login -u $USER --password-stdin"
      sh "docker push jasonkd006/my-repo:${IMAGE_NAME}"
