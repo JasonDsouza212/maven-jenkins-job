dddf1e939fee0b955d550800865bb57fc6e35bf1
Jason <jasondsouza212@gmail.com>
2023-02-06 19:37:49 +0530

    ntg

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,30 @@ provider "aws" {
     region = "ap-south-1"
 }
 
+variable "vpc_cidr_block" {
+    default = "172.41.0.0/16"
+}
+variable "subnet_cidr_block" {
+    default = "172.41.0.0/24"
+}
+variable "avail_zone" {
+    default = "ap-south-1a"
+}
+variable "env_prefix" {
+    default = "dev"
+}
+variable "my_ip" {
+    default = "122.171.18.254/32" 
+}
+variable "jenkins_ip" {
+    default = "122.171.17.217/32"
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "region" {
+  default="ap-south-1a"
+}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
