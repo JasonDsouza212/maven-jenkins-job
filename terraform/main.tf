5b3023e66ba258b5943e23c23b6fac400f50691a
Jason <jasondsouza212@gmail.com>
2023-02-06 19:35:03 +0530

    ntg

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,7 +43,7 @@ resource "aws_default_security_group" "default-myapp-sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = [var.my_ip,var.jenkins_ip]
+    cidr_blocks = [var.my_ip]
   }
 
    ingress{
